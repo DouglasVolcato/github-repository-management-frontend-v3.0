39229fa4071ee67946b6b57ee5b93bb76808d708
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-01 20:36:15 -0300

    chore: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Notes } from "./pages/Notes";
+import { Register } from "./pages/Register";
+import { Repositories } from "./pages/Repositories";
 
-function App() {
-
-  return (
-    <div className="App">
-      <h1>Vite + React</h1>
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/repositories" element={<Repositories />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
