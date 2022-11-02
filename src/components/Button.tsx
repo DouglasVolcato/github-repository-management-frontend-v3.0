56fd83ebe1ccc10352b5d759bac2e738ce7b08f7
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-01 21:35:24 -0300

    feat: add styled button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+interface Props {
+  name: string;
+  onClickFunctions: (() => void)[];
+}
+
+export function Button({ name, onClickFunctions }: Props) {
+  const StyledButton = styled.button`
+    font-size: 15px;
+    margin: 5px;
+    padding: 0.25em 1em;
+    border: 2px solid #0edfdf;
+    border-radius: 3px;
+    color: white;
+    font-weight: bold;
+    background-color: #04810f;
+    width: fit-content;
+    cursor: pointer;
+    &:hover {
+      background-color: #dfca0e;
+    }
+  `;
+
+  return (
+    <StyledButton
+      onClick={(event) => {
+        event.preventDefault();
+        for (const item of onClickFunctions) {
+          item();
+        }
+      }}
+    >
+      {name}
+    </StyledButton>
+  );
+}
