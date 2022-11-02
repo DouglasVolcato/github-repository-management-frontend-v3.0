500e628e84a489d8549b42443f15cd55feec8a95
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-01 23:20:02 -0300

    chore: add style components to register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import { Form } from "../components/Form";
+import { PageTitle } from "../components/PageTitle";
+
 export function Register() {
-  return <div>Register</div>;
+  const [registrationInfo, setRegistrationInfo] = useState({
+    Name: "",
+    Email: "",
+    Password: "",
+    Image: "",
+  });
+
+  function registrate() {
+    alert(
+      registrationInfo.Name +
+        " " +
+        registrationInfo.Email +
+        " " +
+        registrationInfo.Password +
+        " " +
+        registrationInfo.Image
+    );
+  }
+
+  return (
+    <div className="Login">
+      <PageTitle name={"Login"} />
+      <Form
+        state={registrationInfo}
+        setStateFunction={setRegistrationInfo}
+        buttonName={"Submit"}
+        buttonFunction={() => registrate()}
+      />
+    </div>
+  );
 }
