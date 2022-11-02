8f2a600bb54f67ecba5badcee0c5cf44f62d565d
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-01 21:35:45 -0300

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export function Footer() {
+  const StyledFooter = styled.footer`
+    position: fixed;
+    padding: 0.25em 1em;
+    bottom: 0;
+    left: 0;
+    text-align: center;
+    width: 100%;
+    background-color: blue;
+    color: white;
+    font-weight: bold;
+  `;
+
+  return <StyledFooter>Made by Douglas Volcato</StyledFooter>;
+}
