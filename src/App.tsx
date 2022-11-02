2cafed1f93877c5711dc4630ec4947a87bff8bec
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-01 21:36:11 -0300

    feat: add App css style

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 import { Login } from "./pages/Login";
 import { Notes } from "./pages/Notes";
 import { Register } from "./pages/Register";
 import { Repositories } from "./pages/Repositories";
+import "./styles/App.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/notes" element={<Notes />} />
         <Route path="/repositories" element={<Repositories />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
