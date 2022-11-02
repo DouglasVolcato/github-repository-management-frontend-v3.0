59e86a73d5ca385034fc94036c48d94405602324
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-01 21:35:55 -0300

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { Button } from "./Button";
+
+export function Header() {
+  const navigate = useNavigate();
+
+  const StyledHeader = styled.header`
+    /* position: fixed;
+    left: 0;
+    top: 0; */
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    width: 100%;
+    background-color: blue;
+  `;
+
+  return (
+    <StyledHeader className="Header">
+      <Button name="Login" onClickFunctions={[() => navigate("/")]} />
+      <Button
+        name="Register"
+        onClickFunctions={[() => navigate("/register")]}
+      />
+      <Button name="Notes" onClickFunctions={[() => navigate("/notes")]} />
+      <Button
+        name="Repositories"
+        onClickFunctions={[() => navigate("/repositories")]}
+      />
+    </StyledHeader>
+  );
+}
