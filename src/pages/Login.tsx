fb800e5dd78e412041b52ca40a29fb0cc7cebeff
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-01 23:19:51 -0300

    chore: add style components to login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+import { Form } from "../components/Form";
+import { PageTitle } from "../components/PageTitle";
+
 export function Login() {
-  return <div>Login</div>;
+  const [loginInfo, setLoginInfo] = useState({
+    Email: "",
+    Password: "",
+  });
+
+  function makeLogin() {
+    alert(loginInfo.Email + " " + loginInfo.Password);
+  }
+
+  return (
+    <div className="Login">
+      <PageTitle name={"Login"} />
+      <Form
+        state={loginInfo}
+        setStateFunction={setLoginInfo}
+        buttonName={"Submit"}
+        buttonFunction={() => makeLogin()}
+      />
+    </div>
+  );
 }
