a81fa520ce0b7a42db1cfe82391247fd9ca4a180
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-04 13:57:44 -0300

    refactor: remove unused code

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { CardBody } from "../components/CardBody";
 import { NoteCard } from "../components/NoteCard";
 import { PageTitle } from "../components/PageTitle";
