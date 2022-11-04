ad697de0a44911e4471fc453a3d8a2fdaa96d478
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-03 23:52:22 -0300

    fix: add email property

--- src/protocols/user.ts
+++ src/protocols/user.ts
@@ -4,6 +4,7 @@ import { SecurityKey } from "./securityKeys";
 export interface User {
   id: string;
   name: string;
+  email: string;
   password: string;
   photo: string;
   repositories: Note[];
