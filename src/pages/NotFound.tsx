6a3a6f7a697076a9e7e82c0ba5704e52d988990c
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-04 13:56:50 -0300

    feat: add not found image

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,16 @@
 import { PageTitle } from "../components/PageTitle";
+import { UserImage } from "../components/UserImage";
 
 export default function NotFound() {
   return (
     <div>
       <PageTitle name="Error 404:" />
       <PageTitle name="Page not found" />
+      <UserImage
+        link={
+          "https://i.pinimg.com/originals/b3/5e/73/b35e73063c8e30f2bc49f0a2507fa200.jpg"
+        }
+      />
     </div>
   );
 }
